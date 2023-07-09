9f68915428d92978f67ac1d44b86fcedb3468917
Uzair <uzairoomer@live.com>
2023-07-09 18:33:30 +0100

    use axios to fetch api url

--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
 import Card from "../layout/Card"
+import axios from "axios"
 
 function BeerList() {
+  const [beers, setBeers] = useState([]);
+
+  const url = 'https://api.punkapi.com/v2/beers?page=3&per_page=8';
+
+  useEffect(() => {
+    const getBeers = async () => {
+      try {
+        const response = await axios.get(url)
+        setBeers(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getBeers();
+  }, [])
+
   return (
-    <Card />
+    <div class="grid grid-cols-4 gap-4">
+      {beers.map((beer) => (
+        <Card key={beer.id} />
+      ))}
+
+    </div>
   )
 }
 
