b9cba36a579a168170f938eafb5dcfb57baeb731
Uzair <uzairoomer@live.com>
2023-07-09 16:45:06 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-sky-700 text-center">
+      <h2 className="text-white p-8 font-bold text-xl">Oaktyres Test</h2>
     </div>
   );
 }
