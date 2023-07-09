2f16f81b041db92c924fe50180937371f9a11135
Uzair <uzairoomer@live.com>
2023-07-09 18:54:32 +0100

    Format beer cards

--- src/layout/Card.jsx
+++ src/layout/Card.jsx
@@ -2,9 +2,9 @@ function Card({ beers }) {
   return (
     <div class="m-20 max-w-sm rounded overflow-hidden shadow-lg">
         <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">{beers.name}</div>
-            <p class="text-gray-700 text-base">{beers.tagline}</p>
-            <img class="w-24" src={beers.image_url} alt="beer image"></img>
+            <div class="text-center font-bold text-base mb-2">{beers.name}</div>
+            <p class="text-center text-gray-700 text-sm">{beers.tagline}</p>
+            <img class="mx-auto w-16 pt-10" src={beers.image_url} alt="beer image"></img>
         </div>
     </div>
   )
